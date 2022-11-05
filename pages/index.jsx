df7ca7e6b929fdcbcacddfb99f4ebe65d37ee97c
juli <julitati25@gmail.com>
2022-11-05 15:23:55 +0400

    add logic for tab switching

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Panel } from "rsuite";
-import AreaChart from "../components/AreaChart";
+import AnalyticsChart from "../components/AnalyticsAreaChart";
+import AnalyticsBarChart from "../components/AnalyticsBarChart";
+import AnalyticsPieChart from "../components/AnalyticsPieChart";
+import Tabs from "../components/Tabs";
+import { generateFakeProductList } from "../mocks/productsSold";
 import { generateFakeCardList } from "../mocks/reportMock";
 
 const reportCards = generateFakeCardList(3);
+const products = generateFakeProductList(5);
 
 const AnalyticsPage = () => {
-  console.log(reportCards);
+  const [selected, setSelected] = useState("Area Chart");
+
+  const key = `${selected[0].toLowerCase()}${selected.replace(" ", "").slice(1)}`;
+  console.log(key);
+
+  const ChartTypes = {
+    areaChart: {
+      header: "Income",
+      Component: <AnalyticsChart data={reportCards} />
+    },
+    barChart: {
+      header: "Items sold",
+      Component: <AnalyticsBarChart data={reportCards} />
+    },
+    pieChart: {
+      header: "Most sold products",
+      Component: <AnalyticsPieChart data={products} />
+    }
+  };
+  console.log(Object.keys(ChartTypes).map((item) => item));
   return (
-    <div className="flex justify-center p-10">
+    <div className="flex w-full justify-center p-10">
       <div className="space-y-4">
         <div className="text-blue-500 font-bold text-5xl">Check your companys insight!</div>
-        <div className="w-full">
-          <Panel className="w-full h-[17rem] bg-blue-500" header="hi">
-            <AreaChart data={reportCards} />
-          </Panel>
+        <Tabs onChange={setSelected} />
+        <div className="w-full h-[23rem] ">
+          {
+            Object.keys(ChartTypes).includes(key) && (
+              <Panel className="w-full border" header={ChartTypes[key].header}>
+                {ChartTypes[key].Component}
+              </Panel>
+            )
+          }
         </div>
       </div>
     </div>
