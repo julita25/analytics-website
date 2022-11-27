2fe3952b239c7169e47fc2defccb0a9ec60189e1
juli <julitati25@gmail.com>
2022-11-27 09:12:38 +0300

    add dark mode config

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { SelectPicker } from "rsuite";
+import { SelectPicker, Toggle } from "rsuite";
+import { FaSun, FaMoon } from "react-icons/fa";
 import Analytics from "../components/Analytics";
 import Tabs from "../components/Tabs";
 import { pickerData } from "../constants/chartsCostants";
@@ -7,27 +8,39 @@ import { pickerData } from "../constants/chartsCostants";
 const AnalyticsPage = () => {
   const [selected, setSelected] = useState("Area Chart");
   const [value, setValue] = useState("Weekly");
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
-    <div className="flex w-screen justify-center p-10 bg-gray-100 h-screen">
-      <div className="w-[55rem] space-y-10">
-        <div className="text-blue-500 font-bold text-5xl">Company performance</div>
-        <div className="space-y-2">
-          <Tabs onChange={setSelected} />
-          <div className="flex items-center space-x-5">
-            <div>Filter data</div>
-            <SelectPicker
-              disabled={selected === "Pie Chart"}
-              className="w-1/4"
-              searchable={false}
-              value={value}
-              onChange={setValue}
-              data={pickerData}
-            />
+    <div className={`flex flex-col w-screen h-screen ${isDarkMode ? "bg-gray-900" : "bg-gray-200"}`}>
+      <div className="w-full flex justify-end p-5">
+        <Toggle
+          checked={isDarkMode}
+          onChange={(checked) => setIsDarkMode(checked)}
+          size="lg"
+          checkedChildren={<FaMoon className="mt-2 text-gray-300" />}
+          unCheckedChildren={<FaSun className="mt-2 text-yellow-500" />}
+        />
+      </div>
+      <div className="flex justify-center p-10">
+        <div className="w-[55rem] space-y-10">
+          <div className={`font-bold text-5xl ${isDarkMode ? "text-white" : "text-blue-500"}`}>Company performance</div>
+          <div className="space-y-2">
+            <Tabs onChange={setSelected} />
+            <div className="flex items-center space-x-5">
+              <div className={`${isDarkMode ? "text-white" : "text-black"}`}>Filter data</div>
+              <SelectPicker
+                disabled={selected === "Pie Chart"}
+                className="w-1/4"
+                searchable={false}
+                value={value}
+                onChange={setValue}
+                data={pickerData}
+              />
+            </div>
           </div>
-        </div>
-        <div className="h-[30rem] ">
-          <Analytics selected={selected} dateFilter={value} />
+          <div className="h-[30rem] ">
+            <Analytics selected={selected} dateFilter={value} />
+          </div>
         </div>
       </div>
     </div>
