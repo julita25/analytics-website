d51a9eb17185dd0962a9401d9305b6910a26599a
juli <julitati25@gmail.com>
2022-11-27 12:13:35 +0300

    add small cards to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import { FaSun, FaMoon } from "react-icons/fa";
 import Analytics from "../components/Analytics";
 import Tabs from "../components/Tabs";
 import { pickerData } from "../constants/chartsCostants";
+import SmallCard from "../components/SmallCard";
 
 const AnalyticsPage = () => {
   const [selected, setSelected] = useState("Area Chart");
@@ -21,25 +22,45 @@ const AnalyticsPage = () => {
           unCheckedChildren={<FaSun className="mt-2 text-yellow-500" />}
         />
       </div>
-      <div className="flex justify-center p-10">
-        <div className="w-[55rem] space-y-10">
+      <div className="flex flex-col justify-center p-10 space-y-14">
+        <div className="w-full flex justify-center">
           <div className={`font-bold text-5xl ${isDarkMode ? "text-white" : "text-blue-500"}`}>Company performance</div>
-          <div className="space-y-2">
-            <Tabs onChange={setSelected} />
-            <div className="flex items-center space-x-5">
-              <div className={`${isDarkMode ? "text-white" : "text-black"}`}>Filter data</div>
-              <SelectPicker
-                disabled={selected === "Pie Chart"}
-                className="w-1/4"
-                searchable={false}
-                value={value}
-                onChange={setValue}
-                data={pickerData}
-              />
+        </div>
+        <div className="flex justify-between space-x-10 px-10">
+          <div className="space-y-5">
+            <div className="space-y-2 w-[65rem]">
+              <Tabs onChange={setSelected} />
+              <div className="flex items-center space-x-5">
+                <div className={`${isDarkMode ? "text-white" : "text-black"}`}>Filter data</div>
+                <SelectPicker
+                  disabled={selected === "Pie Chart"}
+                  className="w-1/4"
+                  searchable={false}
+                  value={value}
+                  onChange={setValue}
+                  data={pickerData}
+                />
+              </div>
+            </div>
+            <div className="h-[30rem] w-[65rem] justify-center">
+              <Analytics selected={selected} dateFilter={value} />
             </div>
           </div>
-          <div className="h-[30rem] ">
-            <Analytics selected={selected} dateFilter={value} />
+          <div className="w-full flex flex-col justify-center items-center space-y-5 xl:visible invisible">
+            <SmallCard title="Profit margin">
+              {Math.floor(Math.random() * 100) + 1}
+              %
+            </SmallCard>
+            <SmallCard title="Customer satisfaction">
+              {Math.floor(Math.random() * 100) + 1}
+              %
+            </SmallCard>
+            <SmallCard title="# orders on hold">
+              {Math.floor(Math.random() * 20) + 1}
+            </SmallCard>
+            <SmallCard title="# failed orders">
+              {Math.floor(Math.random() * 10) + 1}
+            </SmallCard>
           </div>
         </div>
       </div>
